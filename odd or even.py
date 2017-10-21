0a61fa58291d6085b9e4984091bbbf2490c1a4ac
keerthanaprakash <32951462+balanagarathinam@users.noreply.github.com>
2017-10-21 13:31:06 +0530

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse019
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse019 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ a=int(input("enter the number"))
+ if(a/2==0):
+    print"a is even"
+ else:
+    print"a is odd"
+
+    pass
+
+if __name__ == '__main__':
+    main()
