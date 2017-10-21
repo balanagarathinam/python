9b2c0f2c9af3801e471d0ba69d86dd6f00f6d280
keerthanaprakash <32951462+balanagarathinam@users.noreply.github.com>
2017-10-21 12:03:05 +0530

    Add files via upload

---
+++ finding the data type.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      16cse019
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse019 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+     myString = 34
+     print (type(myString))
+     pass
+
+if __name__ == '__main__':
+    main()
