cd94e1b2b3d4392f11406bff7987fcae0ba3835a
keerthanaprakash <32951462+balanagarathinam@users.noreply.github.com>
2017-10-21 11:37:16 +0530

    Add files via upload

---
+++ age for voting.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      16cse019
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse019 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ a=int(input("enter the age"))
+ if(a>20):
+    print"a is elligeble to vote"
+ elif(a<20):
+    print"a is not elligrble to vote"
+ else:
+    print"good bye"
+    pass
+
+if __name__ == '__main__':
+    main()
